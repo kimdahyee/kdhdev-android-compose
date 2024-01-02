a87c7734e93f9c560984469091d32bf7cfb4ffb5
kimdahyee <diana6777@naver.com>
2024-01-02 19:05:10 +0900

    test commit

--- app/src/main/java/com/example/kdhdevandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/kdhdevandroidcompose/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KdhdevAndroidComposeTheme {
+                // todo kdh
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
